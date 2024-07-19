75a829324026d40bcbc3482dad7a1b36965ce1b8
Chris <52377842+ChrisSulz@users.noreply.github.com>
2024-07-19 22:28:23 +0200

    added invaliv entry (navbar)

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,15 +3,15 @@ import './Navbar.css';
 export default function Navbar() {
     return (
         <nav>
-            <div className="nav-button">
+            <div className="nav-entry">
                 <p onClick={() => { window.location.href = '/'; }}>HOME</p>
             </div>
             <div className='separator'></div>
-            <div className="nav-button">
+            <div className="nav-entry nav-entry-invalid">
                 <p onClick={() => { window.location.href = '/'; }}>MY PROJECTS (tbd)</p>
             </div>
             <div className='separator'></div>
-            <div className="nav-button">
+            <div className="nav-entry">
                 <p onClick={() => { window.location.href = '/about'; }}>ABOUT ME</p>
             </div>
         </nav>
