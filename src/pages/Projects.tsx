aa5a555f99cee036fc1837ef20563e5463950033
Chris <52377842+ChrisSulz@users.noreply.github.com>
2024-07-21 13:55:22 +0200

    added bachelor thesis in projects page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,8 +1,14 @@
 import "./Projects.css";
 import { Helmet } from 'react-helmet';
 import CSCode from "../images/cs-code-logo.svg";
+import BachelorThesis from "../images/icons/bachelor-thesis.svg";
 
-export default function Impressum() {
+export default function Projects() {
+
+  const handleClick1 = () => {
+    window.open('https://github.com/ChrisSulz/StrapiManual', '_blank');
+  };
+
   return (
     <>
       <Helmet>
@@ -13,18 +19,18 @@ export default function Impressum() {
         <br />
         <p className="work-in-progress">- WORK IN PROGRESS -</p>
 
-        <div className="projects-entry">
-          <img src={CSCode} alt="project symbol" className="project-icon" />
+        <div className="projects-entry" onClick={handleClick1} >
+          <img src={BachelorThesis} alt="Bachelor Thesis" className="project-icon" />
           <div className="project-details">
-            <h2 className="project-title">PROJECT PLACEHOLDER 1</h2>
-            <p className="project-descr">Project description placeholder</p>
+            <h2 className="project-title">BACHELORARBEIT</h2>
+            <p className="project-descr">Meine Bachelorarbeit über die <b>automatisierte Generierung</b> eines <b>Benutzerhandbuchs</b> mittels <b>Cypress</b> anhand des Content-Management-Systems <b>Strapi</b>. </p>
           </div>
         </div>
 
         <div className="projects-entry">
           <img src={CSCode} alt="project symbol" className="project-icon" />
           <div className="project-details">
-            <h2 className="project-title">PROJECT PLACEHOLDER 2</h2>
+            <h2 className="project-title">PROJECT PLACEHOLDER</h2>
             <p className="project-descr">Project description placeholder</p>
           </div>
         </div>
